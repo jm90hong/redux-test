0e6256f48de6158c16b34e979a45e1ab2a22b8ad
jm90hong <jm90.hong@gmail.com>
2024-01-24 15:36:43 +0900

    1

--- src/slices/counterSlice.js
+++ src/slices/counterSlice.js
@@ -27,6 +27,11 @@ export const counterSlice = createSlice({
 })
 
 // Action creators are generated for each case reducer function
-export const { setAmount,increment, decrement, incrementByAmount } = counterSlice.actions
+export const { 
+  setAmount,
+  increment, 
+  decrement, 
+  incrementByAmount 
+} = counterSlice.actions
 
 export default counterSlice.reducer
